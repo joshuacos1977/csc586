1a80bf7001bae2589be0e02910f62b4736292a60
joshuacos1977 <coskunolcucu@hotmail.com>
2019-07-25 21:35:12 -0400

    Update basedn_Idf.sh

--- basedn_Idf.sh
+++ basedn_Idf.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+export DEBIAN_FRONTEND='non-interactive'
 
+echo -e "slapd slapd/root_password password admin" |debconf-set-selections
+echo -e "slapd slapd/root_password_again password admin" |debconf-set-selections
+echo -e "slapd slapd/internal/adminpw password test" |debconf-set-selections
+echo -e "slapd slapd/internal/generated_adminpw password admin" |debconf-set-selections
+echo -e "slapd slapd/password2 password admin" |debconf-set-selections
+echo -e "slapd slapd/password1 password admin" |debconf-set-selections
+dpkg-reconfigure slapd
