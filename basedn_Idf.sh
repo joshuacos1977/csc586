917c83b9f73e4aa114df4bfc2b1a515a3d36c837
joshuacos1977 <coskunolcucu@hotmail.com>
2019-07-25 21:44:38 -0400

    Update basedn_Idf.sh

--- basedn_Idf.sh
+++ basedn_Idf.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
-export DEBIAN_FRONTEND='non-interactive'
+dn:
+ou=People,dc=clemson,dc=cloudlab,dc=us
+objectClass: organizationalUnit
+ou: People
+dn:
+ou=Groups,dc=clemson,dc=cloudlab,dc=us
+objectClass: organizationalUnit
+ou: Groups
+dn:
+cn=CSC,ou=Groups,dc=clemson,dc=cloudlab,
+dc=us
+objectClass: posixGroup
+cn: CSC586
+gidNumber: 5000
 
-echo -e "slapd slapd/root_password password admin" |debconf-set-selections
-echo -e "slapd slapd/root_password_again password admin" |debconf-set-selections
-echo -e "slapd slapd/internal/adminpw password test" |debconf-set-selections
-echo -e "slapd slapd/internal/generated_adminpw password admin" |debconf-set-selections
-echo -e "slapd slapd/password2 password admin" |debconf-set-selections
-echo -e "slapd slapd/password1 password admin" |debconf-set-selections
-dpkg-reconfigure slapd
+
