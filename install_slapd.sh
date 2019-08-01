ae398b01c59223ec67e34a83f29967e99a534e16
joshuacos1977 <coskunolcucu@hotmail.com>
2019-08-01 14:03:12 -0400

    Update install_slapd.sh

--- install_slapd.sh
+++ install_slapd.sh
@@ -22,4 +22,31 @@ sudo apt-get install -y slapd ldap-utils
 # Enable firewall rule 
 sudo ufw allow ldap 
 
+# Populate LDAP
+ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w admin -f /local/repository/basedn.ldif
 
+# Generate password hash
+PASS=$(slappasswd -s rammy)
+cat <<EOF >/local/repository/users.ldif
+dn: uid=student,ou=People,dc=clemson,dc=cloudlab,dc=us
+objectClass: inetOrgPerson
+objectClass: posixAccount
+objectClass: shadowAccount
+uid: student
+sn: Ram
+givenName: Golden
+cn: student
+displayName: student
+uidNumber: 10000
+gidNumber: 5000
+userPassword: $PASS
+gecos: Golden Ram
+loginShell: /bin/dash
+homeDirectory: /home/student
+EOF
+
+# Populate LDAP
+ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -w admin -f /local/repository/users.ldif 
+
+# Test LDAP
+ldapsearch -x -LLL -b dc=clemson,dc=cloudlab,dc=us 'uid=student' cn gidNumber
