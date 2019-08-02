e43dac1f645894a04f64ac91d67700c92741888e
joshuacos1977 <coskunolcucu@hotmail.com>
2019-08-02 13:05:40 -0400

    Update install_client.sh

--- install_client.sh
+++ install_client.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 ADMIN_PASSWORD="admin"
-#updates the system repo database
+#updates the system repository database
 sudo apt update
 
-#change the fronentd to noninteractive, avoiding prompts for automation
+#change the frontend to noninteractive for automation
+#Provide the hostname of the node for LDAP server
+#Provide dn for LDAP search base
+#Provide default LDAP version 3
 export DEBIAN_FRONTEND=noninteractive
 echo "
-ldap_auth_config        ldap_auth_config/bindpw password admin
+ldap_auth_config        ldap_auth_config/bindpw password admin    
 ldap_auth_config        ldap_auth_config/rootbindpw password admin
 ldap-auth-config        ldap-auth-config/dbrootlogin    boolean true
 ldap-auth-config        ldap-auth-config/pam_password   select  md5
@@ -21,21 +24,28 @@ ldap_auth_config        ldap_auth_config/override       boolean true " | sudo de
 
 #installs libnss-ldap libpam-ldap ldap-utils along with all their dependencies
 sudo apt install -y -q libnss-ldap -y libpam-ldap ldap-utils
-
-#changes to ldap.conf, ldap.secret, nsswitch.conf, common-session, common-password
-
+#modify IP address
+#modify search base
 sudo sed -i 's/uri ldapi:\/\/\//uri ldap:\/\/192.168.1.1\//g' /etc/ldap.conf
 sudo sed -i 's/base dc=example,dc=net/base dc=clemson,dc=cloudlab,dc=us/g' /etc/ldap.conf
 sudo sed -i 's/rootbinddn cn=manager,dc=example,dc=net/rootbinddn cn=admin,dc=clemson,dc=cloudlab,dc=us/g' /etc/ldap.conf
+#enable LDAP profile for NSS and add ldap  to end of the lines for passwd and group
 sudo sed -i '/passwd:/ s/$/ ldap/' /etc/nsswitch.conf
 sudo sed -i '/group:/ s/$/ ldap/' /etc/nsswitch.conf
+#enable LDAP profile for PAM
+#edit /etc/pam.d/common-sssion and add optional pam_mkhomedir
 sudo sed -i '/# end of pam-auth-update config/ i session optional pam_mkhomedir.so  skel=/etc/skel  umask=077' /etc/pam.d/common-session
+#edit /etc/pam.d/common-password and find line use_authok and remove that phrase from that line
 sudo sed -i 's/use_authtok//g' /etc/pam.d/common-password
 sudo bash <<EOF
+#enter a root pasword to use when ldap-auth-config tries to login to the LDAP directory
 echo $ADMIN_PASSWORD > /etc/ldap.secret
 EOF
+#make the file readable to rrot only
 sudo chmod 600 /etc/ldap.secret
 
-#fetches and prints details for a particular user 
+#auhtenticate student on ldapclient 
 getent passwd student
+
+sudo su - student
 
